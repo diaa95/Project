0ae413aa003ec631c0727b475e3d143bb586c346
Diaa AbdAlDayem <Diaa.abdaldayem@gmail.com>
2020-12-30 22:43:22 +0200

    try commit with intellij

---
+++ src/com/person/Person.java
@@ -0,0 +1,15 @@
+package com.person;
+
+public class Person {
+    private int age;
+    private String name;
+    private static int numberOfPeople = 0;
+    public  Person(int ageParam, String nameParam){
+        age = ageParam;
+        name = nameParam;
+        numberOfPeople++;
+    }
+    public static int peopleCount(){
+        return numberOfPeople;
+    }
+}
